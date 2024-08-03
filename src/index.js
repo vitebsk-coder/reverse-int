b3b44613a2087fa47ffa17a51d6038beb068a84e
Egor <egor.marmilev96@gmail.com>
2024-08-03 12:53:12 +0300

    fix: correct code reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse(n) {
-   return n.toString().split("").reverse().join("");
+   let res = 0;
+   if (n < 0) {
+      res = Math.abs(n);
+      return Number(res.toString().split("").reverse().join(""));
+   } else {
+      return Number(n.toString().split("").reverse().join(""));
+   }
 }
+
